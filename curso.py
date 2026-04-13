28bf008c2d085c4952a4b37f3e2bde795bc1d1b9
JhenifferMenezes <jheniffermenezes818@gmail.com>
2026-04-13 16:02:41 -0300

    Modularização

---
+++ curso.py
@@ -0,0 +1,8 @@
+class Curso:
+    def __init__(self, nome, duracao):
+        self.nome = nome
+        self.duracao = duracao  # em semestres
+
+    def descricao(self):
+        return f"Curso de {self.nome}, duração de {self.duracao} semestres."
+        
